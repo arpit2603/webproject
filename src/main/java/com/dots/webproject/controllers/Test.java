545fa4092bbe3da5c9258742e0947415478fa2fc
arpit2603 <61014468+arpit2603@users.noreply.github.com>
2020-02-14 18:13:34 +0530

    check hook

--- src/main/java/com/dots/webproject/controllers/Test.java
+++ src/main/java/com/dots/webproject/controllers/Test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
 	@GetMapping("/test")
 	public String test(){
-		return "hello";
+		return "hello world";
 	}
 
 }
